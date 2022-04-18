80685c2897b96219c2fb9a5e0d9164b86c9bd872
minsukang <minsu910725@gmail.com>
2022-04-18 09:33:44 +0900

    Add: spa 로직 수정

--- router.js
+++ router.js
@@ -1,29 +1,32 @@
 // template
 const homeTemplate = require('./src/pages/home.hbs');
 const aboutTemplate = require('./src/pages/about.hbs');
+const mainTemplate = require('./src/pages/main.hbs');
 
 const Home = homeTemplate();
 const About = aboutTemplate();
+const Main = mainTemplate();
 
 const routes = {
-  '/': Home,
+  '/': Main,
   '/home': Home,
   '/about': About,
 };
 
 // entry point
 function initialRoutes(mode, el) {
+  console.log(mode, el, window.location.pathname, '라우팅 어려움...');
   renderHTML(el, routes['/']);
-
   if (mode === 'history') {
-    window.onpopstate = () => renderHTML(el, routes[window.location.pathname]);
-  } else {
-    window.addEventListener('hashchange', () => {
-      return renderHTML(el, getHashRoute());
-    });
+    if (window.location.pathname === '/') {
+      renderHTML(el, routes['/']);
+    } else if (window.location.pathname === '/about') {
+      renderHTML(el, routes['/about']);
+    } else {
+      renderHTML(el, routes['/home']);
+    }
   }
 }
-
 // set browser history
 function historyRouterPush(pathName, el) {
   window.history.pushState({}, pathName, window.location.origin + pathName);
